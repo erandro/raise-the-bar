61b5fb24c5425a44af5fb5d2e1563bc7f77001f4
darla <space-age-pete@users.noreply.github.com>
2018-10-08 14:20:40 -0500

    implemented wrappers to dry up code, left it very messy though

--- bartender/src/components/Display/Display.js
+++ bartender/src/components/Display/Display.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "./Display.css";
-import Categories from "../Categories";
-import Drinks from "../Drinks";
+// import Categories from "../Categories";
+// import Drinks from "../Drinks";
+import Wrapper from "../Wrapper";
+import CatButton from "../CatButton";
 
 class Display extends Component {
     state = {
@@ -19,7 +21,7 @@ class Display extends Component {
         console.log(this.firstClickHandler);
         //this.setState({ left: "categories", right: <Categories onClick={this.firstClickHandler} data="squid" id="greenbox" /> });
         //this.setState({ categories: <Categories onClick={this.firstClickHandler} data="squid" array={this.state.array} />, left: "categories", right: <Categories onClick={this.firstClickHandler} data="squid" array={this.state.array} /> });
-        this.setState({ left: "categories", right: "categories" });
+        this.setState({ left: "back", right: "back" });
     }
 
     firstClickHandler = event => {
@@ -39,17 +41,58 @@ class Display extends Component {
         //return <Categories onClick={this.firstClickHandler} data={side} array={this.state.array} />;
         //}
         // else return <h1>{this.state[side]}</h1>
-        const stateSide = this.state[side];
-        switch (stateSide) {
-            case "back":
-            case "categories":
-                return <Categories onClick={this.firstClickHandler} data={side} array={this.state.array2} />;
-            case "orange juice":
-            default:
-                console.log("stateSide: ", stateSide, " side: ", side)
-                return <Drinks onClick={this.firstClickHandler} data={side} array={this.state.categories[stateSide]} />
 
-        }
+        const stateSide = this.state[side];
+
+        // switch (stateSide) {
+        //     case "back":
+        //     case "categories":
+        //         //return <Categories onClick={this.firstClickHandler} data={side} array={this.state.array2} />;
+        //         return (<Wrapper>
+        //             {this.state.array.map(item => (
+        //                 <CatButton key={item} id={item} type={item} onClick={this.firstClickHandler} data={side} />
+        //             ))}
+        //         </Wrapper>)
+        //     case "orange juice":
+        //     default:
+        //         console.log("stateSide: ", stateSide, " side: ", side)
+        //         // return <Drinks onClick={this.firstClickHandler} data={side} array={this.state.categories[stateSide] || []} />;
+        //         let myArray = this.state.categories[stateSide] || [];
+        //         return (
+        //             <Wrapper>
+        //                 <CatButton onClick={this.firstClickHandler} data={side} id={"back"} type={"categories"} />
+        //                 {myArray.map(item => (
+        //                     <CatButton onClick={this.firstClickHandler} data={side} key={item} id={item} type={item} />
+        //                 ))}
+        //             </Wrapper>
+        //         );
+        // }
+
+        let myArray = [];
+
+        // switch (stateSide) {
+        //     case "back":
+        //         //case "categories":
+        //         myArray = this.state.array;
+        //         break;
+        //     case "orange juice":
+        //     default:
+        //         console.log("stateSide: ", stateSide, " side: ", side)
+        //         myArray = this.state.categories[stateSide] || [];
+        //         break;
+        // }
+
+        let drinkArray = this.state.categories[stateSide] || [];
+        myArray = stateSide === "back" ? this.state.array : drinkArray
+
+        return (
+            <Wrapper>
+                <CatButton onClick={this.firstClickHandler} data={side} id={"back"} type={"categories"} />
+                {myArray.map(item => (
+                    <CatButton onClick={this.firstClickHandler} data={side} key={item} id={item} type={item} />
+                ))}
+            </Wrapper>
+        );
     }
 
     render() {
