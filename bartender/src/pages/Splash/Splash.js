78184a7004ec956a6dda6d3a9f3bf0d6911ed2fb
erandro <erandro@gmail.com>
2018-10-15 13:46:18 -0500

    edit the highscore page

--- bartender/src/pages/Splash/Splash.js
+++ bartender/src/pages/Splash/Splash.js
@@ -8,7 +8,7 @@ class Splash extends Component {
         scores: []
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setDB();
     }
 
@@ -16,20 +16,38 @@ class Splash extends Component {
         axios.get("/api/bar/score/")
             .then(res => {
                 console.log(res.data);
-                this.setState({scores: res.data.map(element => <div>{element.name}: {element.points} points</div>)})
+                this.setState({ scores: res.data.map(element => <div>{element.name}: {element.points} points</div>) })
             }
             )
             .catch(err => console.log(err));;
     }
 
-    render(){
-       return (
-            <Jumbotron>
-                <h1>SPLASH</h1>
-                <a href="/">MAIN GAME</a>
-                <h1>{this.state.scores}</h1>
-            </Jumbotron>
-       )
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col id="highScoreTitleBox">
+                        <h1 id="highScoreTitle">[High_Score]</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div id="gameLinkBox">
+                            <a id="gameLink" href="/">BACK_TO_GAME</a>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="2"></Col>
+                    <Col xs="8">
+                        <div id="highScoreNamesBox">
+                            <h1 className="highScoreNames">{this.state.scores}</h1>
+                        </div>
+                    </Col>
+                    <Col xs="2"></Col>
+                </Row>
+            </Container>
+        )
     }
 }
 
